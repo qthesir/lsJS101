1cf5a02d7a24985ba8dea22a2e5c0acc45a68eb6
Quinlan Anderson <quinlan.anderson@gmail.com>
2024-10-26 23:30:59 -0400

    command line book completed

---
+++ lesson1/commandLine/exercisePermission.js
@@ -0,0 +1,17 @@
+// 1. Which user and group are assigned to the /etc folder on your computer?
+// root is the user and wheel is the group
+
+// 2. Which user and group are assigned to the $HOME folder? 
+// My username as the user and staff as the group
+
+// 3. What are mary's permissions for the napkins in the following ouputs?
+// Mary does not have any permissions for the napkins file, which is only available
+// to acmeair, a group Mary is not in.
+
+// 4. What are mary's permissions to the donuts file in the following example?
+// She has read/write access to the donuts file, signified by -rw
+
+// 5. What are Mary's permissions to the laptop file in the following example? What are the permissions for the group acmeinc?
+// Mary has read and execute permissions. Acmeinc has read, write, and execute permissions.
+
+
