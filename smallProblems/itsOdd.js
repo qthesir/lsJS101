8c46a6a4d4e69da3237cc16d64c9b6d4cf86013e
Quinlan Anderson <quinlan.anderson@gmail.com>
2024-10-30 19:23:02 -0400

    small problems

---
+++ smallProblems/itsOdd.js
@@ -0,0 +1,23 @@
+// Write a function that takes one integer argument, which may be positive, negative, or zero. This method returns 
+// true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+
+
+// Pseudocode
+
+// Informal:
+// For a given integer
+// Convert integer to absolute value
+// Check if the value is odd
+// If the value is odd, return true. If the value is even, return false
+
+// Formal:
+
+
+
+
+console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
+console.log(isOdd(-8)); // => false
+console.log(isOdd(0)); // => false
+console.log(isOdd(7)); // => true
