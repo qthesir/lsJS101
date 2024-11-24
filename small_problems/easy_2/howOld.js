5aeeda927a7dd8afb3d25ed08d5d4f962aab3094
Quinlan Anderson <quinlan.anderson@gmail.com>
2024-11-23 19:55:36 -0500

    easy 2 problem 10 complete

--- small_problems/easy_2/howOld.js
+++ small_problems/easy_2/howOld.js
@@ -45,10 +45,37 @@ Algorithm
 const MAX_AGE = 120;
 const MIN_AGE = 20;
 
-function generateAge() {
-  return Math.random() * (MAX_AGE - MIN_AGE) + MIN_AGE;
+function generateAge(min, max) {
+  if (min > max) {
+    console.log("Error: Please enter values where max is greater than min");
+    return NaN;
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let teddyAge = Math.round(generateAge());
+let teddyAge = generateAge(MAX_AGE, MIN_AGE);
 
-console.log(`Teddy is ${teddyAge} years old!`);
+if (!Number.isNaN(teddyAge)) {
+  console.log(`Teddy is ${teddyAge} years old!`);
+}
+
+// Reflection: Could have made this more flexible by having the generate age function
+// take two arguments, the min and the max. Made the change.
+// It's also not clear why the answer key would use floor as opposed to rounding and
+// adding 1. Why not just round? In the worst case, you end up with 0 + the min, which
+// is 20, and the age is in inclusive anyway. I don't think it changes the odds in anyway.
+// You just remove the plus 1 and its good.
+
+// Turns out... that it does change the odds. The numbers at the end of the range are
+// about half as likely to be selected than all of the other integers, because
+// 20.5 or less will round down to 20 and 119.5 or above will round up to 120. So,
+// the floor modification is essential for this function to return each age with
+// equal probability.
+
+// If you gave inputs where the min was greater than the max, well, first of all, this
+// should be guarded against because it's nonsensical and should always be a mistake.
+// If this did happen, though, it would actually return the same range as if they were
+// entered the other way around.
+
+// (20 - 50) + 50
+// (20 - 120) + 120
---
+++ small_problems/easy_2/wenRetire.js
@@ -0,0 +1,82 @@
+/* 
+Build a program that logs when the user will retire and how many more years the user
+has to work until retirement.
+*/
+
+/* 
+PEDAC
+
+Problem
+
+Input: 
+  User selected current age
+  User selected age of retirement
+Output:
+  Current year
+  Year of retirement
+  Number of years before retirement
+
+Implicit Assumptions
+1. The user must enter ages that are greater than 0
+2. The age of retirement must be greater than current age
+3. The program is also logging the year of retirement, and the current year.
+4. I'm assuming that 2017 is the current year and not some different year. 
+5. Current age and retirement age must be rounded to the nearest age if a floating 
+point number is entered.
+6. The program will only accept to whole years, and will not accept months. 
+7. The console output should be wrapped in the text in the example.
+
+Mental Model
+The program asks the user to enter their current age and retirement age, and calculates 
+the number of years remaining until retirement. Then, the program retrieves the current
+year, and adds the number of years remaining until retirement to the current year to get 
+a retirement year. Current year, retirement year, and the number of years until retirement 
+are all logged to the console. 
+
+Examples / Test Cases
+
+Input: 
+  Current Age: 30
+  Retirement Age: 70
+Output:
+  Current Year: 2017
+  Retirement year: 2057
+  Years until retirement: 40
+
+Input: 
+  Current Age: 25
+  Retirement Age: 60
+Output:
+  Current year: 2024
+  Retirement year: 2059
+  Years until retirement: 35 
+
+Data Structure
+
+The program will take two number types as an input, and output string values to the
+console.
+
+Algorithm
+1. SET the variable age, and set it equal to user input (number only)
+2. SET the variable retirementAge, and set it equal to user input
+3. SET yearsToRetirement = retirementAge - age
+4. SET currentYear = current year
+5. SET retirementYear = currentYear + yearsToRetirement
+6. Log currentYear, retirementYear, and yearsToRetirement to the console
+*/
+
+// Code
+
+const readline = require("readline-sync")
+
+
+let age = readline.question("What is your age? ")
+let retirementAge = readline.question("At what age would you like to retire? ")
+
+let yearsToRetirement = retirementAge - age
+
+let currentYear = new Date().getFullYear()
+let retirementYear = currentYear + yearsToRetirement 
+
+console.log(`It's ${currentYear}. You will retire in ${retirementYear}.`)
+console.log(`You only have ${yearsToRetirement} years of work to go!`)
