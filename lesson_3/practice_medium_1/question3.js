bcaa902956fcc9736fa880f7bf5e8d127ced8c3b
Quinlan Anderson <quinlan.anderson@gmail.com>
2024-12-05 00:08:53 -0500

    question 3 medium completed

--- lesson_3/practice_medium_1/question3.js
+++ lesson_3/practice_medium_1/question3.js
@@ -20,3 +20,59 @@ handle it gracefully instead of raising an exception or going into an infinite l
 
 Bonus: What is the purpose of number % divisor === 0 in that code?
 */
+
+/* 
+Explaining the code to myself briefly - the above code is basically taking a number, decrementing that number by
+1 and checking if the next number down is a factor, looping, and doing it until the number = 0. This causes a couple
+problems. The first problem is that, if the number is negative, it causes and infinite loop. This will also
+happen if the number is equal to 0, as it will decrement. 
+*/
+
+function factors2(number) {
+  // insert guard clause
+  if (number <= 0) {
+    return "Error: Please enter a number greater than 0.";
+  }
+
+  let factors = [];
+
+  // use a for loop
+  for (i = number; i > 0; i--) {
+    if (number % i === 0) {
+      factors.push(i);
+    }
+  }
+  return factors;
+}
+
+console.log(factors2(10)); // [10, 5, 2, 1]
+console.log(factors2(4)); // [4, 2, 1]
+console.log(factors2(0)); // Error: Please enter a number greater than 0.
+console.log(factors2(-10)); // Error: Please enter a number greater than 0.
+
+// Bonus: What is the purpose of number % divisor === 0 in that code?
+
+/* The % operator is the modulo operator, which evaluates to the remainder of the left operand divided by 
+the right. If the remainder is 0, then that means the divisor is a factor of number. */
+
+// Lets check Launch School's solution.
+
+function factors3(number) {
+  let factors = [];
+  let divisor = number;
+  while (divisor > 0) {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1
+  }
+  return factors;
+}
+
+console.log(factors3(10)); // [10, 5, 2, 1]
+console.log(factors3(4)); // [4, 2, 1]
+console.log(factors3(0)); // []
+console.log(factors3(-10)); // []
+
+// Ahh. LS solution simply checks, in a while loop, if the divisor is greater than 0. If it is 0 or a negative
+// number, the loop just doesn't run and returns an empty set. This is a lot more clever than my solution. 
